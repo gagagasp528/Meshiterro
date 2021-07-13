6f2192f1309621af969e80f373ad2756f6acf2f7
EC2 Default User <gagagasp528@gmail.com>
2021-07-13 08:37:57 +0000

    Finish4-3

--- config/initializers/application_controller_renderer.rb
+++ config/initializers/application_controller_renderer.rb
@@ -6,3 +6,4 @@
 #     https: false
 #   )
 # end
+Refile.secret_key = 'b41442a770de653dd7c9d630ea4ab7f14fc1cf0de14c1683356b1fe6e77e66c067c19e632a678e06f07650512e9747fadf3bf950dee8f8467b189341145bcffa'
